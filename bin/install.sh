e01871ac2f533719c6c89f4cda5e626604ceeffa
sandai <sandai310@gmail.com>
2012-07-28 19:45:12 +0900

    improve install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# こんにちは
-# なるべくわかりやすくしたかったので、ホームディレクトリ限定でやってます
-# 
-
 readonly HOS="${HOME}/HariboteOS"
 readonly PRO="${HOS}/projects"
 readonly TOL="${HOS}/tolset"
@@ -29,14 +25,12 @@ elif [ ! -e $HOS/Q.app ]; then
 fi
 
 # set z_tools
-
 mkdir -p $ZTO/qemu
 cp $TOL/z_tools/qemu/bios.bin $ZTO/qemu/bios.bin
 cp $TOL/z_tools/qemu/vgabios.bin $ZTO/qemu/vgabios.bin
-mv -R $HOS/Q.app $ZTO/qemu
+mv -f $HOS/Q.app $ZTO/qemu
 
 # set projects
-
 cd $PRO
 find . -maxdepth 1 -regex ".*_day" -type d | xargs -J % cp -rp % ../
 cd ../
@@ -45,18 +39,16 @@ cd ../
 curl -L https://github.com/sandai/30nichideosjisaku/tarball/master | tar xz -C ./
 readonly SANDAI=$(find . -maxdepth 1 -regex ".*sandai-30nichideosjisaku.*" -type d)
 cd $SANDAI
-find . -maxdepth 1 ! -name "bin" -type d | xargs -J % cp -rpf % ../
+find . -regex ".*_day" -o -name "z_tools" -maxdepth 1 -type d | xargs -J % cp -rpf % ../
 cd ../
 
 # move file
 mkdir -p trash
 mv -f $PRO ./trash
 mv -f $TOL ./trash
-rm -rf $SANDAI ./trash
+rm -rf $SANDAI
 cd trash
-touch reademe.txt
 echo "このディレクトリは開発に必要の無いファイル郡です。削除しても構いません。" > readme.txt
-
 
 # All done!
 echo "おわり！がんばってOS作ってね！"
